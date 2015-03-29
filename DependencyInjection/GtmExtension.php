20a1185466591bd04c9467ca1b421e6615160232
Evheniy Bystrov <evheniy.bystrov@gmail.com>
2015-03-29 20:14:59 +0300

    fix DI

--- DependencyInjection/GtmExtension.php
+++ DependencyInjection/GtmExtension.php
@@ -19,6 +19,7 @@ class GtmExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $container->setParameter('gtm', $configs[0]['id']);
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
